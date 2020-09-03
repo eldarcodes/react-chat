1d64aa190fda75133d74edcc5cd39c69fdf5a45f
Eldar Mirzabekov <prog.eldar@gmail.com>
2020-09-04 00:02:00 +0300

    Сделана возможность авторизации на сайт через Google

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './scss/App.scss'
 import Sidebar from './components/Sidebar'
 import Chat from './components/Chat'
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import Login from './components/Login'
+import {useStateValue} from './StateProvider'
 
 const App = () => {
+  const [{user}, dispatch] = useStateValue()
+
   return (
     <div className="app">
-      <div className="app__body">
-        <Router>
-          <Sidebar />
-          <Switch>
-            <Route path="/rooms/:roomId">
-              <Chat />
-            </Route>
-            <Route path="/" exact>
-              {/* <Chat /> */}
-            </Route>
-          </Switch>
-        </Router>
-      </div>
+      {!user ? (
+        <Login />
+      ) : (
+        <div className="app__body">
+          <Router>
+            <Sidebar />
+            <Switch>
+              <Route path="/rooms/:roomId">
+                <Chat />
+              </Route>
+              <Route path="/" exact>
+                {/* <Chat /> */}
+              </Route>
+            </Switch>
+          </Router>
+        </div>
+      )}
     </div>
   )
 }
